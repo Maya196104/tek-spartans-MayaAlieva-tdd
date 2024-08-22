e216520b4389dda51a8bfd6052d540e319793c14
maya <maya.alieva.git@tekschool.us>
2024-08-21 22:42:08 -0500

    Starting Playground with testNG

---
+++ src/test/java/playground/MyFirstTest.java
@@ -0,0 +1,32 @@
+package playground;
+
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class MyFirstTest {
+
+
+    @BeforeMethod
+    public void runBeforeTest(){
+
+        System.out.println("This is Before Each Test");
+
+    }
+
+    @Test
+    public void myFirstTestMethod() {
+  System.out.println("Hello World");
+    }
+
+    @Test
+    public void mySecondTestMethod() {
+        System.out.println("This My second Test");
+    }
+
+     @AfterTest
+    public void runAfterEachTestMethod(){
+        System.out.println("This is running after Each Test");
+
+    }
+}
