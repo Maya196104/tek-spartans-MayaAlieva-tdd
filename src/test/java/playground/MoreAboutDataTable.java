f002c0c1798603263557be5c0b022e23e77c0b34
maya <maya.alieva.git@tekschool.us>
2024-08-25 17:09:22 -0500

    Activity 2 add some tests

---
+++ src/test/java/playground/MoreAboutDataTable.java
@@ -0,0 +1,40 @@
+package playground;
+
+import org.testng.Assert;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+public class MoreAboutDataTable {
+
+    @Test(dataProvider = "testingData1")
+    public void someTesting(String name, Integer expectedLength) {
+
+        Assert.assertEquals(name.length(), expectedLength, "Name length should match");
+
+    }
+
+    @DataProvider(name = "testingData1")
+    public Object[][] testDataWithObject() {
+        return new Object[][] {
+                {"Alijon", 6},
+                {"Salomat", 7},
+                {"Mariela", 7}
+        };
+
+    }
+
+    @DataProvider
+    public Person[] testWithCustomPOJO() {
+        return new Person[] {
+                new Person("Alijon", 6),
+                new Person("Salomat", 7)
+        };
+    }
+
+    @Test(dataProvider = "testWithCustomPOJO")
+    public void someTestingCustomObject(Person person) {
+
+        Assert.assertEquals(person.getName().length(), person.getLength(), "Name length should match");
+
+    }
+}
